51bc2b3b89344954752761bf2f08fd2864997f4a
timkaboya <timothy.kaboya@gmail.com>
2016-11-10 18:06:27 +0200

    Finally resumed and started on work for Week 3

--- src/week3/Change.java
+++ src/week3/Change.java
@@ -2,6 +2,16 @@ package week3;
 
 import java.util.Scanner;
 
+
+/*
+ * Problem: Changing Money!
+ * Explanation: Find the minimum number of coins needed to change the input value (an int)
+ * into coins with denominations of 1, 5, and 10
+ * Input: Single integer m
+ * Output: Min number of coins with denominations 1, 5, 10 that changes m.
+ *
+ * Soln: Design a greedy algorithm for this one.
+ */
 public class Change {
     private static int getChange(int m) {
         //write your code here
